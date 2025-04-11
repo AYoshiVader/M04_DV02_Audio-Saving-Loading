10e6d6b4ba23232e522db7e50cabce9fc4823563
AYoshiVader <andres.quiroga.velarde@gmail.com>
2025-04-11 13:56:16 -0700

    Finish Pause and most of Sliders tutorial

--- SAQV_TerribleTweetersExpanded/Assets/Scripts/PauseMenu.cs
+++ SAQV_TerribleTweetersExpanded/Assets/Scripts/PauseMenu.cs
@@ -1,12 +1,50 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class PauseMenu : MonoBehaviour
 {
+    [SerializeField] AudioMixer _mixer;
+    [SerializeField] string _musicVolume = "MusicVolume";
+    [SerializeField] string _sfxVolume = "SFXVolume";
+    [SerializeField] Slider _musicSlider;
+    [SerializeField] Slider _sfxSlider;
+    [SerializeField] float _multiplier = 30f;
+    
     public static bool GameIsPaused = false;
 
     public GameObject pauseMenuUI;
+
+    private void Awake()
+    {
+        _musicSlider.onValueChanged.AddListener(HandleMusicValueChanged);
+        _sfxSlider.onValueChanged.AddListener(HandleSFXValueChanged);
+    }
+
+    private void OnDisable()
+    {
+        PlayerPrefs.SetFloat(_musicVolume, _musicSlider.value);
+        PlayerPrefs.SetFloat(_sfxVolume, _sfxSlider.value);
+    }
+
+    private void HandleMusicValueChanged(float arg0)
+    {
+        _mixer.SetFloat(_musicVolume, Mathf.Log10(arg0)* _multiplier);
+    }
+
+    private void HandleSFXValueChanged(float arg0)
+    {
+        _mixer.SetFloat(_sfxVolume, Mathf.Log10(arg0) * _multiplier);
+    }
+
+    void Start()
+    {
+        _musicSlider.value = PlayerPrefs.GetFloat(_musicVolume, _musicSlider.value);
+        _sfxSlider.value = PlayerPrefs.GetFloat(_sfxVolume, _sfxSlider.value);
+    }
 
     // Update is called once per frame
     void Update()
@@ -24,17 +62,19 @@ public class PauseMenu : MonoBehaviour
         }
     }
 
-    void Pause()
+    public void Pause()
     {
         pauseMenuUI.SetActive(true);
         Time.timeScale = 0f;
         GameIsPaused = true;
     }
 
-    void Resume()
+    public void Resume()
     {
         pauseMenuUI.SetActive(false);
         Time.timeScale = 1.0f;
         GameIsPaused = false;
     }
+
+//    public 
 }
