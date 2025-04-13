0ff311dadaa451b41ad4dba045b7a26966f4e117
AYoshiVader <andres.quiroga.velarde@gmail.com>
2025-04-12 20:32:32 -0700

    Finish pause menu audio control and Build

--- SAQV_TerribleTweetersExpanded/Assets/Scripts/PauseMenu.cs
+++ SAQV_TerribleTweetersExpanded/Assets/Scripts/PauseMenu.cs
@@ -13,18 +13,42 @@ public class PauseMenu : MonoBehaviour
     [SerializeField] Slider _musicSlider;
     [SerializeField] Slider _sfxSlider;
     [SerializeField] float _multiplier = 30f;
+    [SerializeField] Toggle _musicToggle;
+    [SerializeField] Toggle _sfxToggle;
     
     public static bool GameIsPaused = false;
 
     public GameObject pauseMenuUI;
+    private bool _disableToggleEvent;
 
     private void Awake()
     {
         _musicSlider.onValueChanged.AddListener(HandleMusicValueChanged);
         _sfxSlider.onValueChanged.AddListener(HandleSFXValueChanged);
+        _musicToggle.onValueChanged.AddListener(HandleMusicMuteChanged);
+        _sfxToggle.onValueChanged.AddListener(HandleSFXMuteChanged);
     }
 
-    private void OnDisable()
+    private void HandleMusicMuteChanged(bool enableMusic)
+    {
+        if (enableMusic)
+            _musicSlider.value = _musicSlider.maxValue;
+        else
+            _musicSlider.value = _musicSlider.minValue;
+    }
+
+    private void HandleSFXMuteChanged(bool enableSFX)
+    {
+        if (_disableToggleEvent)
+            return;
+        
+        if (enableSFX)
+            _sfxSlider.value = _sfxSlider.maxValue;
+        else
+            _sfxSlider.value = _sfxSlider.minValue;
+    }
+
+    private void OnPauseDisable()
     {
         PlayerPrefs.SetFloat(_musicVolume, _musicSlider.value);
         PlayerPrefs.SetFloat(_sfxVolume, _sfxSlider.value);
@@ -33,11 +57,17 @@ public class PauseMenu : MonoBehaviour
     private void HandleMusicValueChanged(float arg0)
     {
         _mixer.SetFloat(_musicVolume, Mathf.Log10(arg0)* _multiplier);
+        _disableToggleEvent = true;
+        _musicToggle.isOn = _musicSlider.value > _musicSlider.minValue;
+        _disableToggleEvent = false;
     }
 
     private void HandleSFXValueChanged(float arg0)
     {
         _mixer.SetFloat(_sfxVolume, Mathf.Log10(arg0) * _multiplier);
+        _disableToggleEvent = true;
+        _sfxToggle.isOn = _sfxSlider.value > _sfxSlider.minValue;
+        _disableToggleEvent = false;
     }
 
     void Start()
@@ -72,6 +102,7 @@ public class PauseMenu : MonoBehaviour
     public void Resume()
     {
         pauseMenuUI.SetActive(false);
+        OnPauseDisable();
         Time.timeScale = 1.0f;
         GameIsPaused = false;
     }
